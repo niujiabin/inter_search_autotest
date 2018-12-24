88c974420bad8b1d3b4268a61ad624d9d6ab6147
jiabin.niu <jiabin.niu@qunar.com>
2018-12-24 19:18:10 +0800

    log

---
+++ src/main/java/com/qunar/inter/Application.java
@@ -0,0 +1,15 @@
+package com.qunar.inter;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author jiabin.niu
+ * @date 2018.12
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/qunar/inter/ApplicationTests.java
@@ -0,0 +1,19 @@
+package com.qunar.inter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by jiabin.niu on 2018.12.24 19:16
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
